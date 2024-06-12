bb17a06a544db1d100cc144cee9e3d24003283a1
Sorted1 <87751266+Sorted1@users.noreply.github.com>
2024-06-12 04:20:04 -0400

    z

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     author='sorted1',
     author_email='',
     description='A quick and simple way to integrate keyauth.win and keyauth.cc into your flask applications.',
+    long_description="A quick and simple way to integrate keyauth.win and keyauth.cc into your flask applications.",  # Add the long description here
+    long_description_content_type='text/markdown',  # Specify the content type
     url='https://github.com/sorted1/flask-keyauth',
     project_urls={
         'Website': 'https://sorted.live',
